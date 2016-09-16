e005fe08e9f5e7d6f8ddf176c6a83b479a1632ff
Miranda Mu <mirandamu@gmail.com>
2016-09-16 19:41:43 -0400

    Uploading basic file structure

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Miranda Mu - P2</title>
+    <link rel="stylesheet" href="css/main.css" />
+    <?php require("generator.php"); ?>
+</head>
+<body>
+    
+</body>
+</html>
