855d2ccfc7c17f4ae322af3508f06f6ce5dc38a2
Miranda Mu <mirandamu@gmail.com>
2016-09-20 01:39:48 -0400

    generator with generatepassword function, index with error div, words.txt file

--- generator.php
+++ generator.php
@@ -2,4 +2,52 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-echo "Hello there!";
+function generatepassword($numword, $numcheck, $symcheck) {
+    $pwgen = array();
+    
+    $wordlist = file("words.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $posnum = range(0, 9);
+    $possym = array("!", "@", "#", "$", "%", "&");
+
+    shuffle($wordlist);
+
+    for ($i = 0; $i < $numword; $i++) {
+        array_push($pwgen, $wordlist[$i]);
+    }
+
+    if ($numcheck == "on") {
+        $randnum = array_rand($posnum);
+        array_push($pwgen, $posnum[$randnum]);
+    }
+
+    if ($symcheck == "on") {
+        $randsym = array_rand($possym);
+        array_push($pwgen, $possym[$randsym]);
+    }
+
+    shuffle($pwgen);
+    $newpw = join("-", $pwgen);
+    echo $newpw;
+}
+
+$numword = 0;
+$numcheck = "";
+$symcheck = "";
+
+if(isset($_POST["go"])){
+    if(isset($_POST["numword"])){ 
+    $numword = $_POST["numword"]; 
+        if($numword == "") {
+            $error = "Please enter a number";
+            return;
+        }
+    }
+
+    if(isset($_POST["numcheck"])){ 
+        $numcheck = $_POST["numcheck"]; } 
+
+    if(isset($_POST["symcheck"])){ 
+        $symcheck = $_POST["symcheck"]; } 
+
+    generatepassword($numword, $numcheck, $symcheck);
+}
--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -13,6 +12,17 @@ ini_set('display_errors', 1);
     <?php require("generator.php"); ?>
 </head>
 <body>
-    
+    <form method="POST" action="index.php">  
+        <label for="numword">How many words would you like in your password? (Max 9)</label>
+        <input type="number" name="numword" min="1" max="9"><br>
+        <label for="numcheck">Add a number</label>
+        <input type="checkbox" name="numcheck"><br>
+        <label for="symcheck">Add a special symbol</label>
+        <input type="checkbox" name="symcheck"><br>
+        <input type="submit" name="go" value="Generate your password!"><br>
+    </form>
+    <?php if(isset($error)): ?>
+        <div class="error"><?php echo $error; ?></div>
+    <?php endif ?>
 </body>
 </html>
