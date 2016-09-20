72047c6a0d8a07005849a390e9cfffad791b1658
Miranda Mu <mirandamu@gmail.com>
2016-09-20 14:03:05 -0400

    added form styling, reformatted error msgs

--- generator.php
+++ generator.php
@@ -46,7 +46,7 @@ if(isset($_POST["go"])){
     if(isset($_POST["numWord"])){ 
     $numWord = $_POST["numWord"]; 
         if($numWord == "") {
-            $error = "Please enter a value";
+            $error1 = "Please enter a number";
             return;
         }
         elseif(ctype_digit($numWord) == False) {
@@ -54,7 +54,7 @@ if(isset($_POST["go"])){
             return;
         }
         elseif(($numWord < 1) or ($numWord > 9)) {
-            $error3 = "Please enter a number between 1 and 9";
+            $error3 = "Please enter a max number of 9";
             return;
         }
     }
@@ -64,17 +64,17 @@ if(isset($_POST["go"])){
         if(isset($_POST["numNum"])) {
             $numNum = $_POST["numNum"];
             if(ctype_digit($numNum) == False) {
-                $error4 = "Please enter a number!!";
+                $error4 = "Please enter a number";
                 return;
             }
             elseif($numNum > 10) {
-                $error5 = "Please enter a maximum number of 10!!";
+                $error5 = "Please enter a max number of 10";
                 return;
             }
         } 
     }
     elseif((isset($_POST["numCheck"]) != True) and ($_POST["numNum"] > 0)) {
-        $error6 = "Please check 'add a number'";
+        $error6 = "Please make sure 'Add a number' is checked";
         return;
     }
 
@@ -83,17 +83,17 @@ if(isset($_POST["go"])){
         if(isset($_POST["numSym"])) {
             $numSym = $_POST["numSym"];
             if(ctype_digit($numSym) == False) {
-                $error7 = "Please enter a number!!!!";
+                $error7 = "Please enter a number";
                 return;
             }
             elseif($numSym > 6) {
-                $error8 = "Please enter a maximum number of 6!!";
+                $error8 = "Please enter a max number of 6";
                 return;
             }
         } 
     }
     elseif((isset($_POST["symCheck"]) != True) and ($_POST["numSym"] > 0)) {
-        $error9 = "Please check 'add a symbol'";
+        $error9 = "Please make sure 'Add a symbol' is checked";
         return;
     }
 
--- index.php
+++ index.php
@@ -8,65 +8,80 @@ ini_set('display_errors', 1);
 <head>
     <meta charset="UTF-8">
     <title>Miranda Mu - P2</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/main.css" />
     <?php require("generator.php"); ?>
 </head>
 <body>
-   <p>Required = *</p>
-    <form method="POST" action="index.php">  
-        <label for="numWord">How many words would you like in your password? (Max 9)</label>
-        <input type="number" name="numWord" min="1" max="9"><span> *</span><br>
+   <section>
+    <p>Tired of using your usual password? Looking for some inspiration to keep your sensitive accounts protected? Look no further! Our password generator does the work for you.</p>
+    <ul>
+        <li>Choose how many words you'd like to include in your password. (We recommend at least 2)</li>
+        <li>Additional options include adding numbers or special symbols (e.g. "!", "@", "#", etc.) -- just be sure to check the appropriate box!</li>
+        <li>Required = <strong class="requiredast">***</strong></li>
+    </ul>
+    </section>
+    
+    <h1>Random Password Generator</h1>
+    <form method="POST" action="index.php">
+        <fieldset>
+            <legend>Words</legend>
+            <label for="numWord">How many words would you like in your password? (Max 9)</label>
+            <input type="number" name="numWord" min="1" max="9" required><strong class="requiredast"> ***</strong>
+            <?php if(isset($error)): ?>
+                <span class="error"><?php echo $error1; ?></span>
+            <?php endif ?>
+            <?php if(isset($error2)): ?>
+                <span class="error"><?php echo $error2; ?></span>
+            <?php endif ?>
+            <?php if(isset($error3)): ?>
+                <span class="error"><?php echo $error3; ?></span>
+            <?php endif ?><br><br>
+        </fieldset>
         
-        <label for="numCheck">Add a number</label>
-        <input id="numCheck" type="checkbox" name="numCheck"><br>
+        <fieldset>
+           <legend>Numbers</legend>
+            <label for="numCheck">Add a number</label>
+            <input id="numCheck" type="checkbox" name="numCheck">
+            <?php if(isset($error6)): ?>
+                <span class="error"><?php echo $error6; ?></span>
+            <?php endif ?><br>
+
+            <label for="numNum">How many numbers? (Max 10)</label>
+            <input type="number" name="numNum" min="1" max="10">
+            <?php if(isset($error4)): ?>
+                <span class="error"><?php echo $error4; ?></span>
+            <?php endif ?>
+            <?php if(isset($error5)): ?>
+                <span class="error"><?php echo $error5; ?></span>
+            <?php endif ?><br><br>
+        </fieldset>
         
-        <label for="numNum">How many numbers would you like in your password? (Max 10)</label>
-        <input type="number" name="numNum" min="1" max="10"><br>
-        
-        <label for="symCheck">Add a special symbol</label>
-        <input id="symCheck" type="checkbox" name="symCheck"><br>
-        
-        <label for="numSym">How many symbols would you like in your password? (Max 6)</label>
-        <input type="number" name="numSym" min="1" max="6"><br>
+        <fieldset>
+            <legend>Special Symbols</legend>
+            <label for="symCheck">Add a special symbol</label>
+            <input id="symCheck" type="checkbox" name="symCheck">
+            <?php if(isset($error9)): ?>
+                <span class="error"><?php echo $error9; ?></span>
+            <?php endif ?><br>
+
+            <label for="numSym">How many symbols? (Max 6)</label>
+            <input type="number" name="numSym" min="1" max="6">
+            <?php if(isset($error7)): ?>
+                <span class="error"><?php echo $error7; ?></span>
+            <?php endif ?>
+            <?php if(isset($error8)): ?>
+                <span class="error"><?php echo $error8; ?></span>
+            <?php endif ?><br><br>
+        </fieldset>
         
         <input type="submit" name="go" value="Generate your password!"><br>
     </form>
-    <?php if(isset($error)): ?>
-        <div class="error"><?php echo $error; ?></div>
-    <?php endif ?>
-    <?php if(isset($error2)): ?>
-        <div class="error"><?php echo $error2; ?></div>
-    <?php endif ?>
-    <?php if(isset($error3)): ?>
-        <div class="error"><?php echo $error3; ?></div>
-    <?php endif ?>
-    <?php if(isset($error4)): ?>
-        <div class="error"><?php echo $error4; ?></div>
-    <?php endif ?>
-    <?php if(isset($error5)): ?>
-        <div class="error"><?php echo $error5; ?></div>
-    <?php endif ?>
-    <?php if(isset($error6)): ?>
-        <div class="error"><?php echo $error6; ?></div>
-    <?php endif ?>
-    <?php if(isset($error7)): ?>
-        <div class="error"><?php echo $error7; ?></div>
-    <?php endif ?>
-    <?php if(isset($error8)): ?>
-        <div class="error"><?php echo $error8; ?></div>
-    <?php endif ?>
-    <?php if(isset($error9)): ?>
-        <div class="error"><?php echo $error9; ?></div>
-    <?php endif ?>
+    
     <br><br>
-    <table>
-        <tr>
-            <td>placeholder</td>
-        </tr>
-        <tr>
-            <td>another</td>
-            <td>one</td>
-        </tr>
-    </table>
 </body>
 </html>
